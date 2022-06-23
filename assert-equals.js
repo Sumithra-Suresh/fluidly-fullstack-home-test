869905777566a12004f0b728bce6acc55a10bb15
Sumithra-Suresh <jasisumi@gmail.com>
2022-06-23 19:04:16 +0100

    Added test cases for take home challenge

--- assert-equals.js
+++ assert-equals.js
@@ -1,3 +1,49 @@
-function assertEquals(expect, actual) {}
 
-module.exports = assertEquals
+//=========================================================================================================
+// assertEquals(expect, actual)
+// Compares the expect and actual parameters. 
+// 
+//=========================================================================================================
+
+function assertEquals(expect, actual) {
+    if(expect !== actual){
+        throw new Error();
+    }
+    return true;
+  }
+  
+  //=========================================================================================================
+  // arrayEquals(expect, actual)
+  // Compares the two arrays are equal or not.
+  // 1. If length is not as expected, throw an error.
+  // 2. Compare every element, return true for success and error otherwise.
+  //
+  //=========================================================================================================
+  
+  function arrayEquals(expect, actual) {
+     
+    if (expect.length === actual.length) {
+  
+      return expect.every((element, index) => {
+  
+        if (element === actual[index]) {
+          return true;
+        }
+        
+        throw new Error();
+      });
+  
+    }
+    throw new Error();
+  }
+  
+  //=========================================================================================================
+  // module exports
+  // exports the functions to be reusable in other files.
+  // 
+  //=========================================================================================================
+  
+  module.exports = {
+    assertEquals,
+    arrayEquals
+  }
--- assert-equals.spec.js
+++ assert-equals.spec.js
@@ -1,13 +1,70 @@
-const assertEquals = require('./assert-equals')
+
+// Include the necessary modules from other files
+
+const modules = require('./assert-equals')
+
+//=========================================================================================================
+// Testing assertEquals()
+// Comparing strings and numbers with and without throwing errors
+// 
+//=========================================================================================================
 
 describe('assertEquals', () => {
-  describe('when expected and actual are the same string', () => {
-    it('returns without throwing an error', () => {
-      expect(() => assertEquals('abc', 'abc')).not.toThrow()
-    })
+  
+  describe('when expected and actual are the same', () => {
+
+    it('Comparing strings : returns without throwing an error', () => {
+      expect(() => modules.assertEquals('abc', 'abc')).not.toThrow()
+    });
+
+    it('Comparing numbers : returns without throwing an error', () => {
+      expect(() => modules.assertEquals(1, 1)).not.toThrow()
+    });
+
   })
 
-  describe('when expected and actual are different strings', () => {
-    it.todo('throws an error')
+  describe('when expected and actual are different', () => {
+    it('Expected "abcef" but found "abc"', () => {
+      expect(() => modules.assertEquals('abcef', 'abc')).toThrow();
+    });
+
+    it('Expected 1 but found 2', () => {
+      expect(() => modules.assertEquals(1, 2)).toThrow();
+    });
+
+    it( 'Expected type number but found type string', () => {
+      expect(() => modules.assertEquals(1, '1')).toThrow();
+    });
+
+  })
+})
+
+
+//=========================================================================================================
+// Testing arrayEquals()
+// Comparing arrays with and without throwing errors
+// 
+//=========================================================================================================
+
+describe('arrayEquals', () => {
+
+  describe('when expected and actual are the same', () => {
+
+    it('Comapring arrays : returns without throwing an error', () => {
+      expect(() => modules.arrayEquals(['a','b','c'], ['a','b','c'])).not.toThrow()
+    });
+
+  })
+
+  describe('when expected and actual are different', () => {
+
+    it( 'Expected array length 2 but found 3', () => {
+      expect(() => modules.arrayEquals(['a','b'], ['a','b','c'])).toThrow();
+    });
+
+    it( 'Expected "b" but found "d"', () => {
+      expect(() => modules.arrayEquals(['a','b'], ['a','d'])).toThrow();
+    });
+
   })
 })
